d738fe273da5d9c837430f6c4e89c45b792dd95f
Caique Moreira <caique.moreira@movile.com>
2019-12-15 09:57:14 -0300

    Created simple module for serving a health API Also added structure for Account sample module

---
+++ ktor-sample-account-handling/src/main/kotlin/com/kiq83/ktor/sample/account/Main.kt
@@ -0,0 +1,5 @@
+package com.kiq83.ktor.sample.account
+
+fun main(args: Array<String>) {
+    println("foi")
+}
---
+++ ktor-sample-health-api/src/main/kotlin/com/kiq83/ktor/sample/SimpleHealthMain.kt
@@ -0,0 +1,20 @@
+package com.kiq83.ktor.sample
+
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+
+    embeddedServer(Netty, 8383) {
+        routing {
+            get("/health") {
+                call.respondText("I'm a healthy server")
+            }
+        }
+    }.start(wait = true)
+
+}
